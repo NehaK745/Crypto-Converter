2d0149034615e32f9c6edeaae436b9e9fb450bf4
NehaK745 <nehakumari1461@gmail.com>
2021-12-15 21:18:49 +0530

    first commit

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
 function Currency() {
-    const currencies = ['BTC', 'ETH', 'USD', 'XRP', 'LTC', 'ADA' ]
-    const [amount, setAmount] = useState("BTC");
-    const [firstcurruncy, setfirstCurruncy] = useState('BTC')
-    const [secondcurruncy, setsecondCurruncy] = useState('BTC')
+  const currencies = ["BTC", "ETH", "USD", "XRP", "LTC", "ADA"];
+  const [amount, setAmount] = useState("BTC");
+  const [firstcurruncy, setfirstCurruncy] = useState("BTC");
+  const [secondcurruncy, setsecondCurruncy] = useState("BTC");
   return (
     <div className="currency">
       <h2>Currency Converter</h2>
@@ -14,42 +14,46 @@ function Currency() {
             <tr>
               <td>From</td>
               <td>
-                <input type="number"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                name="currency-1" />
+                <input
+                  type="number"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                  name="currency-1"
+                />
               </td>
               <td>
                 <select name="currency-A">
-                value{firstcurruncy}
-                className="currency-A"
-                onChange={(e) => setfirstCurruncy(e.target.value)}
-                {currencies.map(currency => (
+                  value{firstcurruncy}
+                  className="currency-A" onChange=
+                  {(e) => setfirstCurruncy(e.target.value)}
+                  {currencies.map((currency) => (
                     <option key={currency} value={currency}>
-                    {currency}
+                      {currency}
                     </option>
-                ))}
+                  ))}
                 </select>
               </td>
             </tr>
             <tr>
               <td>To</td>
               <td>
-                <input type="number"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                name="currency-B" />
+                <input
+                  type="number"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                  name="currency-B"
+                />
               </td>
               <td>
                 <select name="currency-B">
-                value{secondcurruncy}
-                className="currency-B"
-                onChange={(e) => setsecondCurruncy(e.target.value)}
-                {currencies.map(currency => (
+                  value{secondcurruncy}
+                  className="currency-B" onChange=
+                  {(e) => setsecondCurruncy(e.target.value)}
+                  {currencies.map((currency) => (
                     <option key={currency} value={currency}>
-                    {currency}
+                      {currency}
                     </option>
-                ))}
+                  ))}
                 </select>
               </td>
             </tr>
